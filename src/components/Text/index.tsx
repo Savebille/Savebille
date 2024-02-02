ee82fbc65f023ea00cbd032d6553d855723598f0
Daniel Gómez <danielivgho5@gmail.com>
2024-02-02 14:20:03 -0500

    feat: adjust navbar styles and layout

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,48 +1,52 @@
-import { getTextColor, getTextSize, getTextWeight } from "../../shared/utils/styles.utils";
+import {
+  getTextColor,
+  getTextSize,
+  getTextWeight,
+} from '../../shared/utils/styles.utils';
 interface Props {
-	children: React.ReactNode;
-	color?:
-		| 'info'
-		| 'success'
-		| 'yellow'
-		| 'error'
-		| 'primary'
-		| 'secondary'
-		| 'gray'
-		| 'white'
-	size?:
-		| 'mini'
-		| 'mini-2'
-		| 'extraSmall'
-		| 'small'
-		| 'medium'
-		| 'large'
-		| 'extraLarge'
-		| 'extraextraLarge'
-		| 'mostLarge';
-	weight?: 'light' | 'regular' | 'medium' | 'bold';
-	sx?: string;
-	beforeIcon?: React.ReactNode;
+  children: React.ReactNode;
+  color?:
+    | 'info'
+    | 'success'
+    | 'yellow'
+    | 'error'
+    | 'primary'
+    | 'secondary'
+    | 'gray'
+    | 'white';
+  size?:
+    | 'mini'
+    | 'mini-2'
+    | 'extraSmall'
+    | 'small'
+    | 'medium'
+    | 'large'
+    | 'extraLarge'
+    | 'extraextraLarge'
+    | 'mostLarge';
+  weight?: 'light' | 'regular' | 'medium' | 'semibold' | 'bold';
+  sx?: string;
+  beforeIcon?: React.ReactNode;
 }
 
 const Text = ({
-	children,
-	color = 'primary',
-	size = 'medium',
-	sx,
-	weight = 'regular',
-	beforeIcon,
+  children,
+  color = 'primary',
+  size = 'medium',
+  sx,
+  weight = 'regular',
+  beforeIcon,
 }: Props) => {
-	return (
-		<p
-			className={`leading-5 flex gap-2 items-center  ${getTextColor(
-				color,
-			)} ${getTextSize(size)} ${getTextWeight(weight)} ${sx}`}
-		>
-			{beforeIcon}
-			{children}
-		</p>
-	);
+  return (
+    <p
+      className={`leading-5 flex gap-2 items-center  ${getTextColor(
+        color
+      )} ${getTextSize(size)} ${getTextWeight(weight)} ${sx}`}
+    >
+      {beforeIcon}
+      {children}
+    </p>
+  );
 };
 
 export default Text;
--- src/shared/utils/styles.utils.ts
+++ src/shared/utils/styles.utils.ts
@@ -1,68 +1,65 @@
 const getTextColor = (textColor: string) => {
-	const colors: Record<string, string> = {
-		info: 'text-h-info',
-		success: 'text-h-success',
-		yellow: 'text-h-yellow',
-		error: 'text-h-error',
-		primary: 'text-h-primary',
-		secondary: 'text-h-secondary',
-		gray: 'text-h-gray',
-		white: 'text-h-white',
-	};
+  const colors: Record<string, string> = {
+    info: 'text-h-info',
+    success: 'text-h-success',
+    yellow: 'text-h-yellow',
+    error: 'text-h-error',
+    primary: 'text-h-primary',
+    secondary: 'text-h-secondary',
+    gray: 'text-h-gray',
+    white: 'text-h-white',
+  };
 
-	return colors[textColor] || 'text-title';
+  return colors[textColor] || 'text-title';
 };
 
 const getTextSize = (textSize: string) => {
-	switch (textSize) {
-		case 'mini':
-			return 'text-[11px]';
-		case 'mini-2':
-			return 'text-[12px]';
-		case 'extraSmall':
-			return 'text-[13px]';
-		case 'small':
-			return 'text-sm';
-		case 'medium':
-			return 'text-base';
-		case 'large':
-			return 'text-lg';
-		case 'extra-large':
-			return 'text-xl';
-		case 'extra-extra-large':
-			return 'text-2xl';
-		case 'most-large':
-			return 'text-[28px]';
-		default:
-			return 'text-base';
-	}
+  switch (textSize) {
+    case 'mini':
+      return 'text-[11px]';
+    case 'mini-2':
+      return 'text-[12px]';
+    case 'extraSmall':
+      return 'text-[13px]';
+    case 'small':
+      return 'text-sm';
+    case 'medium':
+      return 'text-base';
+    case 'large':
+      return 'text-lg';
+    case 'extra-large':
+      return 'text-xl';
+    case 'extra-extra-large':
+      return 'text-2xl';
+    case 'most-large':
+      return 'text-[28px]';
+    default:
+      return 'text-base';
+  }
 };
 
 const getTextWeight = (weight: string) => {
-	switch (weight) {
-		case 'light':
-			return 'font-light';
-		case 'regular':
-			return 'font-regular';
-		case 'medium':
-			return 'font-medium';
-		case 'bold':
-			return 'font-bold';
-		default:
-			return 'font-regular';
-	}
+  switch (weight) {
+    case 'light':
+      return 'font-light';
+    case 'regular':
+      return 'font-regular';
+    case 'medium':
+      return 'font-medium';
+    case 'semibold':
+      return 'font-semibold';
+    case 'bold':
+      return 'font-bold';
+    default:
+      return 'font-regular';
+  }
 };
 
 const getBgColorRow = (index: number) => {
-	if (index % 2 === 0) {
-		return 'bg-white';
-	} else {
-		return 'bg-[#FAFAFA]';
-	}
+  if (index % 2 === 0) {
+    return 'bg-white';
+  } else {
+    return 'bg-[#FAFAFA]';
+  }
 };
-export {
-	getTextColor,
-	getTextSize,
-	getTextWeight,
-	getBgColorRow,
-};
+export { getTextColor, getTextSize, getTextWeight, getBgColorRow };
