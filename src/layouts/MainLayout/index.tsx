2492d9be920a77347895e9b2587c6ef429676212
Martin <martin.rubiano1234@gmail.com>
2024-02-05 16:57:18 -0500

    Responsive Navbar

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import CollapsibleSidebar from '../../components/CollapsibleSidebar';
 import { Bell, MagnifyingGlass, ArrowDown } from '@phosphor-icons/react';
 import Text from '../../components/Text';
+import Input from './components/input';
+
 
 const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   const isDesktop = window.innerWidth > 768;
@@ -14,48 +16,39 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
             !isDesktop ? 'hidden md:w-0' : 'md:w-0'
           }`}
         >
-          <CollapsibleSidebar />
+          <CollapsibleSidebar/>
         </div>
       )}
 
       {/* TODO : NavBar */}
-      <div className='flex flex-col w-full relative'>
+      <div className='flex flex-col w-full relative '>
         <div
-          className={`flex items-center justify-center bg-white  ${
-            !isDesktop ? 'sticky top-0 md:static' : 'sticky top-0'
-          } z-40`}
+          className={`flex items-center justify-center bg-white ${!isDesktop ? 'sticky top-0 md:static' : 'sticky top-0'} z-40`}
         >
-          <div className='flex justify-between items-center  w-full z-[150] ml-60 p-9 h-[100px] border-b border-h-gray '>
+          <div className='flex justify-between items-center  w-full z-[150] p-9 h-[100px] border-b border-h-gray xl:ml-60'>
             {/* Leftside */}
-            <div className='bg-h-gray-input flex items-center justify-between p-2 w-[450px] rounded-md h-[42px]'>
-              <div className='flex items-center justify-between h-[20px] w-[320px] gap-2'>
-                <div className='flex justify-center items-center'>
-                  <MagnifyingGlass color='#233145' size={20} />
-                </div>
-                <input
-                  type='text'
-                  placeholder='Type here to search'
-                  className='bg-h-gray-input h-full w-full border-h-info focus:outline-none text-h-secondary text-sm'
+            <div className='xl:hidden'>
+              <img 
+               src="https://cdn.iconscout.com/icon/premium/png-512-thumb/finance-1433977-1212011.png?f=webp&w=256"
+               alt="bill"
+               width={42}
+               height={42}
                 />
-              </div>
-
-              <div className='bg-h-white h-[32px] w-[52px] rounded-lg py-1 flex justify-center items-center shadow'>
-                <Text size='medium' weight='semibold' color='primary'>
-                  ⌘ F
-                </Text>
-              </div>
             </div>
+            
+          {/* Input */}
+           <Input />
 
             {/* Rightside */}
             <div className='bg-h-white flex items-center justify-between w-auto h-[42px]'>
               {/* user-account */}
               <div className='flex items-center'>
-                <div className='bg-h-gray-input rounded-full h-[42px] w-[42px] flex items-center justify-center mr-8 border-b  border-h-gray'>
+                <div className='bg-h-gray-input rounded-full h-[42px] w-[42px] flex items-center justify-center mx-4 border-b  border-h-gray'>
                   <Bell size={20} />
                 </div>
 
                 {/* user-info */}
-                <div className='w-[310px] h-[42px] flex justify-between items-center'>
+                <div className='w-[310px] h-[42px] xl:flex xl:justify-between xl:items-center'>
                   <div className='flex items-center gap-3'>
                     <img
                       src='src/assets/img/LoginAvatar.png'
@@ -63,7 +56,7 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
                       className='w-[42px] h-[42px] rounded-full'
                     />
 
-                    <div className='flex flex-col h-[38px] justify-between'>
+                    <div className='lg:flex lg:flex-col lg:h-[38px] lg:justify-between lg:gap-1 hidden'>
                       <Text size='large' weight='bold' color='primary'>
                         Martin Rubiano
                       </Text>
@@ -71,11 +64,11 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
                         martin.savebille@gmail.com
                       </Text>
                     </div>
+                    <div className='lg:w-[28px] lg:h-[28px] lg:justify-content lg:items-center lg:block hidden'>
+                      <ArrowDown size={20} color='#8E98A7' />
+                    </div>
                   </div>
 
-                  <div className='w-[28px] h-[28px] flex justify-center items-center '>
-                    <ArrowDown size={20} color='#8E98A7' />
-                  </div>
                 </div>
               </div>
             </div>
