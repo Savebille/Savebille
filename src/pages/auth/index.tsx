d160ec987963f182b33175eab2d6ea5756037c10
Daniel Gómez <danielivgho5@gmail.com>
2024-02-06 16:27:21 -0500

    create sign in and sign up pages responsive

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -7,7 +7,7 @@ const Login: React.FC = () => {
     <div className='h-screen w-screen flex items-center justify-center'>
       {/* main container */}
       <div className='bg-h-white flex flex-col-reverse lg:flex-row justify-between rounded-3xl shadow-lg max-w-[1438px] max-h-[954px] w-[90%] h-[80%]'>
-        {/* Sign Up Form */}
+        {/* Sign In Form */}
         <div className='w-full h-[80%]  lg:w-[58%] lg:h-full p-[30px] lg:p-[80px] xl:px-[120px] flex flex-col items-center justify-between'>
           {/* header */}
           <div className='w-full h-auto flex flex-col items-center justify-between gap-4'>
@@ -21,23 +21,18 @@ const Login: React.FC = () => {
             </div>
 
             {/* textos */}
-            <div className='w-full h-auto flex flex-col items-center justify-between text-center'>
+            <div className='w-full h-auto flex flex-col items-center justify-between text-center gap-2'>
               <Text weight='medium' color='primary' size='title'>
-                Welcome to Savebille
+                Welcome back
               </Text>
-              <Text
-                weight='regular'
-                color='secondary'
-                size='extraLarge'
-                sx='mt-1'
-              >
-                Create a new account.
+              <Text weight='regular' color='secondary' size='extraLarge'>
+                Please enter your details to sign in.
               </Text>
             </div>
           </div>
 
           {/* buttons */}
-          <div className='w-full h-auto flex flex-col justify-between lg:flex-row lg:justify-center items-center  gap-2'>
+          <div className='w-full h-auto flex flex-col justify-between lg:flex-row lg:justify-center items-center gap-2'>
             <button className='w-full h-[50px] lg:w-[50%] rounded-[10px] border-[1.5px] border-h-gray hover:bg-h-gray-input flex justify-center items-center shadow transition ease-in-out hover:scale-105 duration-200'>
               <img src='src/assets/img/google.svg' alt='google icon' />
             </button>
@@ -55,27 +50,15 @@ const Login: React.FC = () => {
           </div>
 
           {/* form */}
-          <div className='w-full h-auto flex flex-col justify-between items-center gap-5'>
+          <div className='w-full h-auto flex flex-col justify-between items-center gap-6'>
             {/* inputs */}
-            <form className='w-full h-auto flex flex-col justify-between gap-5'>
-
-              {/* name */}
-              <div className='flex flex-col gap-2'>
-                <label htmlFor='name' className='text-h-primary hidden lg:block'>
-                  First name
-                </label>
-                <input
-                  type='text'
-                  name='name'
-                  id='name'
-                  placeholder='What is your name?'
-                  className='w-full h-[50px] border-[1px] border-h-gray rounded-[10px] px-4 bg-h-gray-input focus:outline-none'
-                />
-              </div>
-
+            <form className='w-full h-auto flex flex-col justify-between gap-6'>
               {/* email */}
               <div className='flex flex-col items-start gap-2'>
-                <label htmlFor='email' className='text-h-primary hidden lg:block'>
+                <label
+                  htmlFor='email'
+                  className='text-h-primary hidden lg:block'
+                >
                   Email
                 </label>
                 <input
@@ -89,7 +72,10 @@ const Login: React.FC = () => {
 
               {/* password */}
               <div className='flex flex-col items-start gap-2'>
-                <label htmlFor='password' className='text-h-primary hidden lg:block'>
+                <label
+                  htmlFor='password'
+                  className='text-h-primary hidden lg:block'
+                >
                   Password
                 </label>
                 <div className='relative w-full'>
@@ -109,15 +95,15 @@ const Login: React.FC = () => {
             </form>
 
             {/* remember password */}
-            <div className='w-full h-auto flex flex-col sm:flex-row justify-start items-center gap-1'>
+            <div className='w-full h-auto flex flex-col sm:flex-row justify-between items-center gap-2'>
               <label className='w-auto h-auto flex items-center text-h-secondary text-base gap-2'>
                 <input type='checkbox' className='checkbox-input' />
-                <Text color='secondary'>You agree to our</Text>
+                <Text color='secondary'>Remember me</Text>
               </label>
 
               <a href='#' className='w-auto h-auto flex items-center'>
                 <Text color='info' sx='underline' size='medium'>
-                  Terms and Conditions
+                  Forgot password?
                 </Text>
               </a>
             </div>
@@ -126,14 +112,14 @@ const Login: React.FC = () => {
           {/* Button SignIn */}
           <button className='w-full h-[50px] rounded-xl bg-h-info flex justify-center items-center transition ease-in-out hover:scale-105 duration-200'>
             <Text weight='medium' color='white' size='extraLarge'>
-              Register
+              Sign in
             </Text>
           </button>
 
           {/* Sign Up */}
           <div className='w-full h-auto flex flex-col justify-between sm:flex-row sm:justify-center items-center text-center gap-2'>
             <Text color='secondary' weight='regular' size='medium'>
-              Do you already have an account?
+              Don't have an account yet?
             </Text>
             <a href='#'>
               <Text
@@ -142,12 +128,12 @@ const Login: React.FC = () => {
                 size='medium'
                 sx='hover:text-h-info transition duration-200'
               >
-                Login
+                Sign Up
               </Text>
             </a>
           </div>
         </div>
-        {/* img */}
+        ;{/* img */}
         <div className='w-full h-[20%] lg:w-[42%] lg:h-full overflow-hidden'>
           <img
             src='src/assets/img/LoginBanner.png'
@@ -163,28 +149,23 @@ const Login: React.FC = () => {
 export default React.memo(Login);
 
 () => {
+  
+};
+
+() => {
   {
-    /* Sign In Form */
+    /* Sign Up Form */
   }
   <div className='w-full h-[80%]  lg:w-[58%] lg:h-full p-[30px] lg:p-[80px] xl:px-[120px] flex flex-col items-center justify-between'>
     {/* header */}
-    <div className='w-full h-auto flex flex-col items-center justify-between gap-4'>
-      {/* avatar */}
-      <div className='w-[70px] h-[70px] lg:w-[90px] lg:h-[90px] overflow-hidden rounded-full shadow-xl'>
-        <img
-          src='src/assets/img/LoginAvatar.png'
-          alt='user avatar'
-          className='object-cover object-center'
-        />
-      </div>
-
+    <div className='w-full h-auto flex flex-col items-center'>
       {/* textos */}
-      <div className='w-full h-auto flex flex-col items-center justify-between text-center'>
+      <div className='w-full h-auto flex flex-col items-center justify-between text-center gap-2'>
         <Text weight='medium' color='primary' size='title'>
-          Welcome back
+          Create a new account
         </Text>
-        <Text weight='regular' color='secondary' size='extraLarge' sx='mt-1'>
-          Please enter your details to sign in.
+        <Text weight='regular' color='secondary' size='extraLarge'>
+          Please enter your details to sign up.
         </Text>
       </div>
     </div>
@@ -208,43 +189,68 @@ export default React.memo(Login);
     </div>
 
     {/* form */}
-    <div className='w-full h-auto flex flex-col justify-between items-center gap-8 sm:gap-4'>
+    <div className='w-full h-auto flex flex-col justify-between items-center gap-6 '>
       {/* inputs */}
-      <form className='w-full h-auto flex flex-col justify-between gap-8'>
+      <form className='w-full h-auto flex flex-col justify-between gap-6'>
+        {/* name */}
+        <div className='flex flex-col gap-2'>
+          <label htmlFor='name' className='text-h-primary hidden lg:block'>
+            First name
+          </label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='What is your name?'
+            className='w-full h-[50px] border-[1px] border-h-gray rounded-[10px] px-4 bg-h-gray-input focus:outline-none'
+          />
+        </div>
 
         {/* email */}
-        <input
-          type='email'
-          name='user-email'
-          placeholder='Enter your email...'
-          className='w-full h-[50px] border-[1px] border-h-gray rounded-[10px] px-4 bg-h-gray-input focus:outline-none'
-        />
+        <div className='flex flex-col items-start gap-2'>
+          <label htmlFor='email' className='text-h-primary hidden lg:block'>
+            Email
+          </label>
+          <input
+            type='email'
+            name='user-email'
+            id='email'
+            placeholder='What is your email?'
+            className='w-full h-[50px] border-[1px] border-h-gray rounded-[10px] px-4 bg-h-gray-input focus:outline-none'
+          />
+        </div>
 
         {/* password */}
-        <div className='relative'>
-          <input
-            type='password'
-            name='user-password'
-            placeholder='Enter your password...'
-            className='w-full h-[50px] border-[1px] border-h-gray rounded-[10px] pl-4 pr-12 bg-h-gray-input focus:outline-none'
-          />
-          <Eye
-            className='absolute top-1/2 -translate-y-3 right-4 text-h-secondary'
-            size={24}
-          />
+        <div className='flex flex-col items-start gap-2'>
+          <label htmlFor='password' className='text-h-primary hidden lg:block'>
+            Password
+          </label>
+          <div className='relative w-full'>
+            <input
+              type='password'
+              name='user-password'
+              id='password'
+              placeholder='Enter your password...'
+              className='w-full h-[50px] border-[1px] border-h-gray rounded-[10px] pl-4 pr-12 bg-h-gray-input focus:outline-none'
+            />
+            <Eye
+              className='absolute top-1/2 -translate-y-3 right-4 text-h-secondary'
+              size={24}
+            />
+          </div>
         </div>
       </form>
 
       {/* remember password */}
-      <div className='w-full h-auto flex flex-col sm:flex-row justify-between items-center gap-2'>
-        <label className='w-auto h-auto flex items-center text-h-secondary text-base'>
-          <input type='checkbox' className='mr-2 checkbox-input' />
-          <Text color='secondary'>Remember me</Text>
+      <div className='w-full h-auto flex flex-col sm:flex-row justify-start items-center gap-1'>
+        <label className='w-auto h-auto flex items-center text-h-secondary text-base gap-2'>
+          <input type='checkbox' className='checkbox-input' />
+          <Text color='secondary'>You agree to our</Text>
         </label>
 
         <a href='#' className='w-auto h-auto flex items-center'>
           <Text color='info' sx='underline' size='medium'>
-            Forgot password?
+            Terms and Conditions
           </Text>
         </a>
       </div>
@@ -253,14 +259,14 @@ export default React.memo(Login);
     {/* Button SignIn */}
     <button className='w-full h-[50px] rounded-xl bg-h-info flex justify-center items-center transition ease-in-out hover:scale-105 duration-200'>
       <Text weight='medium' color='white' size='extraLarge'>
-        Sign in
+        Register
       </Text>
     </button>
 
     {/* Sign Up */}
     <div className='w-full h-auto flex flex-col justify-between sm:flex-row sm:justify-center items-center text-center gap-2'>
       <Text color='secondary' weight='regular' size='medium'>
-        Don't have an account yet?
+        Do you already have an account?
       </Text>
       <a href='#'>
         <Text
@@ -269,7 +275,7 @@ export default React.memo(Login);
           size='medium'
           sx='hover:text-h-info transition duration-200'
         >
-          Sign Up
+          Login
         </Text>
       </a>
     </div>
