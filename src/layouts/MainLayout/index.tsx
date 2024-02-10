41ef9c2ee896818bbe5a5eeeb84f7437ae3bbce6
Daniel Gómez <danielivgho5@gmail.com>
2024-02-09 20:01:10 -0500

    adjust main layout

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -14,21 +14,16 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
       </div>
 
       <div className='flex flex-col w-full relative'>
-        <div className='bg-h-white w-full border-b border-h-gray'>
-
-          <div className='flex w-full lg:hidden p-4 sticky justify-between'>
-            <div className='flex gap-4 ml-2'>
+        <div className='bg-h-white w-full sticky top-0'>
+          <div className='flex w-full lg:hidden px-4 py-2 lg:p-4 justify-between '>
+            <div className='flex gap-3'>
               <img
                 src='https://cdn.iconscout.com/icon/premium/png-512-thumb/finance-1433977-1212011.png?f=webp&w=256'
                 alt='avatar'
                 width={50}
                 height={50}
               />
-              <Text
-                color='info'
-                size='title'
-                weight='bold'
-              >
+              <Text color='info' size='title' weight='bold'>
                 Savebille
               </Text>
             </div>
@@ -40,11 +35,12 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
               <img
                 src='src/assets/img/LoginAvatar.png'
                 alt='logo'
-                className='w-[42px] h-[42px] rounded-full mr-2' />
+                className='w-[42px] h-[42px] rounded-full mr-2'
+              />
             </div>
           </div>
           {/* Input Mobile */}
-          <div className='bg-h-white p-4 mx-2 lg:hidden' >
+          <div className='bg-h-white px-4 py-2 lg:hidden'>
             <div className='bg-h-gray-input flex-start items-center place-content-center p-2 rounded-md h-[42px]lg:hidden xl:hidden'>
               <div className='flex items-center justify-between h-[20px] gap-3'>
                 <div className='flex justify-center items-center'>
@@ -53,18 +49,17 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
                 <input
                   type='text'
                   placeholder='Type here to search'
-                  className='bg-h-gray-input h-full border-h-info focus:outline-none text-h-secondary text-sm w-full px-1 ' />
+                  className='bg-h-gray-input h-full border-h-info focus:outline-none text-h-secondary text-sm w-full px-1 '
+                />
               </div>
             </div>
           </div>
         </div>
 
-
-        <div className='bg-h-white lg:flex hidden justify-between items-center h-[100px] border-b border-h-gray'>
+        <div className='bg-h-white lg:flex hidden justify-between items-center h-[100px] border-b border-h-gray ml-60 p-6 gap-6'>
           {/* Leftside */}
-
           {/* Input */}
-          <div className='bg-h-gray-input lg:flex lg:items-center lg:justify-between p-2 rounded-md h-[47px] lg:w-[432px] md:hidden hidden ml-[270px]'>
+          <div className='bg-h-gray-input lg:flex lg:items-center lg:justify-between p-2 rounded-md h-[47px] lg:w-[400px] md:hidden hidden'>
             <div className='flex items-center justify-between h-[20px] lg:w-auto gap-2'>
               <div className='flex justify-center items-center md:block'>
                 <MagnifyingGlass color='#233145' size={25} />
@@ -72,7 +67,8 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
               <input
                 type='text'
                 placeholder='Type here to search'
-                className='bg-h-gray-input h-full w-full border-h-info focus:outline-none text-h-secondary text-sm' />
+                className='bg-h-gray-input h-full w-full border-h-info focus:outline-none text-h-secondary text-sm'
+              />
             </div>
 
             <div className='bg-h-white h-[32px] min-w-[52px] rounded-lg p-2 shadow md:hidden lg:block'>
@@ -83,22 +79,23 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
           </div>
 
           {/* Rightside */}
-          <div className='bg-h-white flex items-center justify-between w-auto h-[42px]'>
+          <div className='bg-h-white flex items-center justify-between w-auto h-11'>
             {/* user-account */}
-            <div className='lg:flex items-center hidden'>
-              <div className='bg-h-gray-input rounded-full h-[42px] w-[42px] flex items-center justify-center mx-4 border-b border-h-gray'>
+            <div className='lg:flex items-center hidden gap-4'>
+              <button className='rounded-full h-11 w-11 flex items-center justify-center border-b border-h-gray'>
                 <Bell size={20} />
-              </div>
+              </button>
 
               {/* user-info */}
-              <div className='w-[310px] h-[42px] xl:flex xl:justify-between xl:items-center'>
+              <div className='w-auto h-full xl:flex xl:justify-between xl:items-center'>
                 <div className='flex items-center gap-3'>
                   <img
                     src='src/assets/img/LoginAvatar.png'
-                    alt=''
-                    className='w-[42px] h-[42px] rounded-full hidden lg:block' />
+                    alt='userAvatar'
+                    className='w-11 h-11 rounded-full hidden lg:block'
+                  />
 
-                  <div className='lg:flex-col lg:h-[38px] lg:gap-1 lg:block hidden'>
+                  <div className='hidden lg:flex lg:flex-col lg:h-[38px] gap-1'>
                     <Text size='large' weight='bold' color='primary'>
                       Martin Rubiano
                     </Text>
@@ -106,18 +103,13 @@ const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
                       martin.savebille@gmail.com
                     </Text>
                   </div>
-                  <div className='lg:w-[28px] lg:h-[28px] lg:items-center lg:block hidden'>
-                    <ArrowDown size={20} color='#8E98A7' />
-                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <main
-          className={`flex-1 h-full pb-14 md:pb-0 ml-0 lg:ml-60`}
-        >
+        <main className={`flex-1 h-full pb-14 md:pb-0 ml-0 lg:ml-60`}>
           <MobileMenu />
           {children}
         </main>
