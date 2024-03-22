dc91c8b9007a3f3c0f3764589eb670b491311a89
Sepudev <sepulvedandres8@gmail.com>
2024-03-22 15:56:32 -0500

    category selector adjustments

--- src/components/CategorySelector/index.tsx
+++ src/components/CategorySelector/index.tsx
@@ -1,30 +1,31 @@
-import { FormControl, FormLabel } from '@/components/ui/form';
+import { FormControl, FormLabel } from "@/components/ui/form";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select';
+} from "@/components/ui/select";
 
-import { ReactNode } from 'react';
-import { BookmarkSimple } from '@phosphor-icons/react';
-import Text from '../Text';
-import CustomLoader from '../shared/CustomLoader';
-import { Categories } from '@/_root/pages/categories';
-import { getColorByName, getIconByName } from '@/shared/utils/general.utils';
+import { ReactNode } from "react";
+import { BookmarkSimple } from "@phosphor-icons/react";
+import Text from "../Text";
+import CustomLoader from "../shared/CustomLoader";
+import { Categories } from "@/_root/pages/categories";
+import { getColorByName, getIconByName } from "@/shared/utils/general.utils";
+import Divider from "../Divider";
 
 export interface Option {
   icon: ReactNode;
   name: string;
   color:
-    | 'red'
-    | 'purple'
-    | 'blue'
-    | 'darkGreen'
-    | 'lightGreen'
-    | 'yellow'
-    | 'orange';
+    | "red"
+    | "purple"
+    | "blue"
+    | "darkGreen"
+    | "lightGreen"
+    | "yellow"
+    | "orange";
 }
 
 interface selectorProps {
@@ -41,9 +42,9 @@ const CategorySelector = ({
   isLoadingCategories,
 }: selectorProps) => {
   return (
-    <div className='flex items-center gap-4'>
+    <div className="flex items-center gap-4">
       <FormLabel>
-        <BookmarkSimple size={24} color='#8e98a7' />
+        <BookmarkSimple size={24} color="#8e98a7" />
       </FormLabel>
 
       <Select
@@ -51,16 +52,64 @@ const CategorySelector = ({
         defaultValue={fieldProps.value}
       >
         <FormControl>
-          <SelectTrigger className='w-full h-6 text-h-secondary'>
-            <SelectValue placeholder='Categoria' />
+          <SelectTrigger className="w-full h-6 text-h-secondary">
+            <SelectValue placeholder="Categoria" />
           </SelectTrigger>
         </FormControl>
 
-        <SelectContent className='max-h-48 overflow-y-auto'>
+        <SelectContent className="max-h-72 overflow-y-auto p-2">
+          {isLoadingCategories ? (
+            <div className="mt-10">
+              <CustomLoader color="#3183ff" height={44} width={44} />
+            </div>
+          ) : (
+            <>
+              {userOptions.length > 0 && (
+                <>
+                  <Text size="text-1" weight="medium" sx="mt-3 mb-3">
+                    Mis categorías
+                  </Text>
+
+                  <Divider />
+                  {userOptions.map((item: Categories) => (
+                    <SelectItem
+                      key={item.name}
+                      value={`${item.icon},${item.name},${item.color}`}
+                    >
+                      <FormLabel>
+                        <div className="flex items-center">
+                          <div
+                            className={`${item.color} flex items-center justify-center rounded-full w-7 h-7`}
+                          >
+                            {getIconByName(item.icon)}
+                          </div>
+                          <Text
+                            color="primary"
+                            weight="medium"
+                            size="h5"
+                            sx="ml-4"
+                          >
+                            {item.name}
+                          </Text>
+                        </div>
+                      </FormLabel>
+                    </SelectItem>
+                  ))}
+                </>
+              )}
+            </>
+          )}
+
+          <Text size="text-1" weight="medium" sx="mt-3 mb-3">
+            Por defecto
+          </Text>
+
+          <Divider />
+
           {defaultOptions.map((item: Option) => (
             <SelectItem key={item.name} value={item.name}>
               <FormLabel>
-                <div className='flex items-center'>
+                <div className="flex items-center">
                   <div
                     className={`${getColorByName(
                       item.color
@@ -68,39 +117,13 @@ const CategorySelector = ({
                   >
                     {item.icon}
                   </div>
-                  <Text color='primary' weight='medium' size='h5' sx='ml-4'>
+                  <Text color="primary" weight="medium" size="h5" sx="ml-4">
                     {item.name}
                   </Text>
                 </div>
               </FormLabel>
             </SelectItem>
           ))}
-
-          {isLoadingCategories ? (
-            <div className='mt-10'>
-              <CustomLoader color='#3183ff' height={44} width={44} />
-            </div>
-          ) : (
-            userOptions.map((item: Categories) => (
-              <SelectItem
-                key={item.name}
-                value={`${item.icon},${item.name},${item.color}`}
-              >
-                <FormLabel>
-                  <div className='flex items-center'>
-                    <div
-                      className={`${item.color} flex items-center justify-center rounded-full w-7 h-7`}
-                    >
-                      {getIconByName(item.icon)}
-                    </div>
-                    <Text color='primary' weight='medium' size='h5' sx='ml-4'>
-                      {item.name}
-                    </Text>
-                  </div>
-                </FormLabel>
-              </SelectItem>
-            ))
-          )}
         </SelectContent>
       </Select>
     </div>
